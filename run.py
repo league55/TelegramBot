87149f9c1baf4803ec104534a0cd967bbc7bb003
mrudenko <m.rudenko@zaleos.net>
2018-12-02 23:36:39 +0100

    Deploy

--- run.py
+++ run.py
@@ -7,10 +7,12 @@ from flask import Flask, request
 app = Flask(__name__)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+
+TOKEN = os.environ['BOT_TOKEN']
 
 global bot
-bot = telegram.Bot(token=os.environ['BOT_TOKEN'])
+bot = telegram.Bot(token=TOKEN)
 
 def echo(update):
     chat_id = update.message.chat.id
@@ -27,6 +29,7 @@ def command_to_action(update):
 def webhook_handler():
     if request.method == "POST":
         # retrieve the message in JSON and then transform it to Telegram object
+        print(request.get_json())
         update = telegram.Update.de_json(request.get_json(force=True), bot)
 
         action = command_to_action(update)
