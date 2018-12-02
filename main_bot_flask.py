e1b0b39a45fde84010aa6d793d0dbfdd5b08d697
mrudenko <m.rudenko@zaleos.net>
2018-12-02 22:24:28 +0100

    Deploy

---
+++ main_bot_flask.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import telegram
+from flask import Flask, request
+
+from secret.config import TOKEN
+
+if __name__ == "__main__":
+    app = Flask(__name__)
+    app.run()
+
+global bot
+bot = telegram.Bot(token=TOKEN)
+
+def echo(update):
+    chat_id = update.message.chat.id
+    text = update.message.text.encode('utf-8')
+
+    bot.sendMessage(chat_id=chat_id, text=text)
+
+
+def command_to_action(update):
+    return echo
+
+
+
+@app.route('/HOOK', methods=['POST'])
+def webhook_handler():
+    if request.method == "POST":
+        # retrieve the message in JSON and then transform it to Telegram object
+        update = telegram.Update.de_json(request.get_json(force=True))
+
+        action = command_to_action(update)
+        action(update)
+
+    return 'ok'
+
+
+@app.route('/set_webhook', methods=['GET', 'POST'])
+def set_webhook():
+    s = bot.setWebhook('https://localhost:5000/HOOK')
+    if s:
+        return "webhook setup ok"
+    else:
+        return "webhook setup failed"
+
+
+@app.route('/')
+def index():
+    return '.'
