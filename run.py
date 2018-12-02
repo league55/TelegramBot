e3604632d9d3ea5aad4871db5c9acc91f4f28648
mrudenko <m.rudenko@zaleos.net>
2018-12-02 23:20:35 +0100

    fix args error

--- run.py
+++ run.py
@@ -27,7 +27,7 @@ def command_to_action(update):
 def webhook_handler():
     if request.method == "POST":
         # retrieve the message in JSON and then transform it to Telegram object
-        update = telegram.Update.de_json(request.get_json(force=True))
+        update = telegram.Update.de_json(request.get_json(force=True), bot)
 
         action = command_to_action(update)
         action(update)
